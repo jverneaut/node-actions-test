ea2acffc1d8d42a000cd96895bfba3adde3ba7d8
Julien Verneaut <jverneaut@gmail.com>
2019-10-15 13:45:34 +0200

    Test

--- .github/actions/deploy/entrypoint.sh
+++ .github/actions/deploy/entrypoint.sh
@@ -1,6 +1,14 @@
 #!/bin/sh -l
 
-echo "TEST1"
-echo "$SSH_KEY"
-echo "TEST2"
-echo $SSH_KEY
+echo "$SSH_KEY" > "$HOME/.ssh/deploy_key"
+
+chmod 700 "$HOME/.ssh"
+chmod 600 "$HOME/.ssh/known_hosts"
+chmod 600 "$HOME/.ssh/deploy_key"
+
+eval $(ssh-agent)
+ssh-add "$HOME/.ssh/deploy_key"
+
+ssh-keyscan -t rsa 68.183.76.87 >> "$HOME/.ssh/known_hosts"
+
+ssh -o StrictHostKeyChecking=no -A -tt -p ${PORT:-22} root@68.183.76.87 "$*"
